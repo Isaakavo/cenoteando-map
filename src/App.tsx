1f3a4a915329e361de37f67bb692f1b4e30e4ed1
isaakavo <isaakhaas96@gmail.com>
2022-12-17 14:37:39 -0600

    feat: adding clusters

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,38 @@ import { MapC } from './components/MapC';
 import RemoteServices from './services/RemoteServices';
 import CenoteDTO from './models/CenoteDTO';
 
+export interface geoJsonI {
+  type: 'Feature';
+  geometry: {
+    type: 'Point';
+    coordinates: [number, number];
+  };
+}
+
 function App() {
   const [cenotes, setCenotes] = React.useState<CenoteDTO[] | null>(null);
-
+  const [geoJson, setGeoJson] = React.useState<geoJsonI[]>([]);
   const fetching = async () => {
-    let response = RemoteServices.cenotesGenerator(5);
+    let response = RemoteServices.cenotesGenerator(500);
     for await (let res of response) {
       setCenotes(res);
+      const newData = [];
+      for (const data of res) {
+        const type = data.geojson.type;
+        const coordinates = data.geojson.geometry.coordinates
+        const coordinatesType = data.geojson.geometry.type
+        const newObj = {
+            type,
+            geometry: {
+              type: coordinatesType,
+              coordinates
+            }
+          ,
+
+        }
+        newData.push(newObj);
+      }
+      setGeoJson(newData);
     }
   };
 
@@ -20,7 +45,7 @@ function App() {
   }, []);
 
   //console.log({cenotes});
-  
+
   return (
     <div className='map-wrap'>
       <MapC
@@ -28,6 +53,7 @@ function App() {
         lat={20.96670000000006}
         zoom={10}
         cenotes={cenotes}
+        geoJson={geoJson}
       />
     </div>
   );
--- src/components/MapC.tsx
+++ src/components/MapC.tsx
@@ -1,6 +1,8 @@
-import maplibreGl, { Map } from 'maplibre-gl';
+import { Point } from 'geojson';
+import maplibreGl, { GeoJSONSource, LngLatLike, Map } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import React from 'react';
+import { geoJsonI } from '../App';
 import '../map.css';
 import CenoteDTO from '../models/CenoteDTO';
 import { layers, mapLayers } from '../utils/tiles';
@@ -11,10 +13,11 @@ interface MapCI {
   lat: number;
   zoom: number;
   cenotes?: CenoteDTO[] | null;
+  geoJson: geoJsonI[];
 }
 
 export const MapC: React.FC<MapCI> = (props) => {
-  let { cenotes } = props;
+  let { cenotes, geoJson } = props;
 
   const mapContainer = React.useRef(null);
   const map = React.useRef<Map | null>(null);
@@ -23,10 +26,13 @@ export const MapC: React.FC<MapCI> = (props) => {
   const [lat] = React.useState(20.96670000000006);
   const [zoom] = React.useState(props.zoom);
   const [API_KEY] = React.useState('2ovqIDOtsFG069J69Ap2');
+  // const [cenotesCoor, setCenotesCoor] = React.useState<[number, number][]>([]);
 
-  const onSelectedOptionCallback = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const onSelectedOptionCallback = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     map.current?.setStyle(mapLayers(e.target.value));
-  }
+  };
 
   React.useEffect(() => {
     if (map.current) return; //stops map from intializing more than once
@@ -38,18 +44,154 @@ export const MapC: React.FC<MapCI> = (props) => {
     });
     let nav = new maplibreGl.NavigationControl({});
     map.current.addControl(nav, 'bottom-right');
-  }, [zoom]);
+  }, [geoJson]);
 
   React.useEffect(() => {
-    cenotes?.forEach((elem) => {
-      debugger;
-      if (map.current !== null) {
-        new maplibreGl.Marker({ color: '#1b691b' })
-          .setLngLat(elem.geojson.geometry.coordinates)
-          .addTo(map.current);
-      }
-    });
-  }, [cenotes]);
+    if (map.current) {
+      map.current.on('load', () => {
+        if (geoJson.length > 0) {
+          map.current?.addSource('cenotes', {
+            type: 'geojson',
+            // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+            // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+            data: {
+              type: 'FeatureCollection',
+              features: geoJson,
+            },
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+          });
+
+          map.current?.addLayer({
+            id: 'clusters',
+            type: 'circle',
+            source: 'cenotes',
+            paint: {
+              // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+              // with three steps to implement three types of circles:
+              //   * Blue, 20px circles when point count is less than 100
+              //   * Yellow, 30px circles when point count is between 100 and 750
+              //   * Pink, 40px circles when point count is greater than or equal to 750
+              'circle-color': [
+                'step',
+                ['get', 'point_count'],
+                '#51bbd6',
+                100,
+                '#f1f075',
+                750,
+                '#f28cb1',
+              ],
+              'circle-radius': [
+                'step',
+                ['get', 'point_count'],
+                20,
+                100,
+                30,
+                750,
+                40,
+              ],
+            },
+          });
+
+          map.current?.addLayer({
+            id: 'cluster-count',
+            type: 'symbol',
+            source: 'cenotes',
+            filter: ['has', 'point_count'],
+            layout: {
+              'text-field': ['get', 'point_count_abbreviated'],
+              'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+              'text-size': 12,
+            },
+          });
+
+          map.current?.addLayer({
+            id: 'unclustered-point',
+            type: 'circle',
+            source: 'cenotes',
+            filter: ['!', ['has', 'point_count']],
+            paint: {
+              'circle-color': '#12730d',
+              'circle-radius': 10,
+              'circle-stroke-width': 5,
+              'circle-stroke-color': '#fff',
+            },
+          });
+        }
+      });
+
+      map.current.on('click', 'clusters', (e) => {
+        debugger;
+        const features = map.current?.queryRenderedFeatures(e.point, {
+          layers: ['clusters'],
+        })[0];
+        if (features !== undefined) {
+          const clusterId = features.properties['cluster_id'];
+
+          if (!clusterId) return;
+
+          (
+            map.current?.getSource('cenotes') as GeoJSONSource
+          ).getClusterExpansionZoom(clusterId, (err, zoom) => {
+            if (err) return;
+
+            if (map.current) {
+              map.current.easeTo({
+                center: (features.geometry as Point).coordinates as LngLatLike,
+                zoom: zoom ? zoom : undefined,
+              });
+            }
+          });
+        }
+      });
+
+      // When a click event occurs on a feature in
+      // the unclustered-point layer, open a popup at
+      // the location of the feature, with
+      // description HTML from its properties.
+      map.current.on('click', 'unclustered-point', (e) => {
+        debugger;
+        if (e.features !== undefined) {
+          const coordinates = (
+            e.features[0].geometry as Point
+          ).coordinates.slice();
+          const mag = e.features[0]?.properties?.mag;
+          const tsunami =
+            e.features[0]?.properties?.tsunami === 1 ? 'yes' : 'no';
+          // Ensure that if the map is zoomed out such that
+          // multiple copies of the feature are visible, the
+          // popup appears over the copy being pointed to.
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
+        }
+      });
+
+      map.current.on('mouseenter', 'clusters', () => {
+        if (map.current) {
+          map.current.getCanvas().style.cursor = 'pointer'
+        }
+      });
+
+      map.current.on('mouseleave', 'clusters', () => {
+        if (map.current) {
+          map.current.getCanvas().style.cursor = ''
+        }
+      });
+    }
+
+    // cenotes?.forEach((elem) => {
+    //   const newCenotes = [elem.geojson.geometry.coordinates, ...cenotesCoor]
+    //   setCenotesCoor(newCenotes);
+    //   // if (map.current !== null) {
+    //   //   new maplibreGl.Marker({ color: '#1b691b' })
+    //   //     .setLngLat(elem.geojson.geometry.coordinates)
+    //   //     .addTo(map.current);
+    //   // }
+    // });
+    // console.log({cenotesCoor});
+  }, [geoJson]);
 
   return (
     <div className='map-wrap'>
